8bd2fcb91096c758191fcb2c491fd5f4e8c21197
Fedora-MISTAKELY <thereisamailforme@gmail.com>
2025-08-06 09:35:10 +0545

    Fix Prisma configuration - remove Data Proxy requirement

--- servers/gql/modules/user/userResolver.ts
+++ servers/gql/modules/user/userResolver.ts
@@ -5,35 +5,45 @@ import { GraphQLContext } from "../../context";
 export const userResolvers = {
   Query: {
     getUser: async (_: any, __: any, ctx: GraphQLContext) => {
-      const { clerkId } = ctx.user!;
-      const userResponse = await prisma.user.findUnique({
-        where: { clerkId },
-        include: {
-          events: true,
-          projects: true,
-          resources: true,
-          polls: true,
-          votes: true,
-        },
-      });
+      try {
+        const { clerkId } = ctx.user!;
+        const userResponse = await prisma.user.findUnique({
+          where: { clerkId },
+          include: {
+            events: true,
+            projects: true,
+            resources: true,
+            polls: true,
+            votes: true,
+          },
+        });
 
-      console.log("user response-->", userResponse);
-      return userResponse;
+        console.log("user response-->", userResponse);
+        return userResponse;
+      } catch (error) {
+        console.log("error while getting user-->", error);
+        console.error(error);
+      }
     },
     getUsers: async (
       _: any,
       { limit, offset }: { limit?: number; offset?: number },
       ctx: GraphQLContext
     ) => {
-      const user = requireAuth(ctx);
-      if(!user)throw new Error("login required") 
-      const userResponse = await prisma.user.findMany({
-        take: limit || 10,
-        skip: offset || 0,
-      });
+      try {
+        const user = requireAuth(ctx);
+        if (!user) throw new Error("login required");
+        const userResponse = await prisma.user.findMany({
+          take: limit || 10,
+          skip: offset || 0,
+        });
 
-      console.log(userResponse);
-      return userResponse;
+        console.log(userResponse);
+        return userResponse;
+      } catch (error) {
+        console.log("error while getting user-->", error);
+        console.error(error);
+      }
     },
   },
   Mutation: {
